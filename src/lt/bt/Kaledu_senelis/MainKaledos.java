a7797a522724ae3ae19ce4a0b20b0b7d6a2c409a
Kostas <qostaz@yahoo.com>
2018-12-11 22:40:38 +0200

    Kaledos3

--- src/lt/bt/Kaledu_senelis/MainKaledos.java
+++ src/lt/bt/Kaledu_senelis/MainKaledos.java
@@ -5,107 +5,42 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-import lt.bt.Kaledu_senelis.Charakteristika;
+
 
 public class MainKaledos {
 
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub		
-		//C:\\Users\\kosta\\eclipse-workspace\\Uzduotys\\res\\Kaledos\\Elfu_sandelis.txt
-		//C:\\Users\\kosta\\eclipse-workspace\\Uzduotys\\res\\Kaledos\\Vaiko_charakteristika.txt
-		//C:\\Users\\kosta\\eclipse-workspace\\Uzduotys\\res\\Kaledos\\Vaikų_norai.txt
-
 
 		List <Zaislas> anglis = new ArrayList<Zaislas>();
 		List<Zaislas> zaisluSandelys = new ArrayList<Zaislas>();
 		Scanner reader = Utils.startScanner("C:\\Users\\kosta\\eclipse-workspace\\Uzduotys\\res\\Kaledos\\Elfu_sandelis.txt");
-		reader.nextLine();
-		while(reader.hasNext()) {
-			String[] tempArray = reader.nextLine().split(",");
-			if (!tempArray[0].trim().equals("Anglių maišas")) {
-				zaisluSandelys.add(new Zaislas(tempArray[0].trim(), Integer.parseInt(tempArray[1].trim())));
-			} else {
-				anglis.add(new Zaislas(tempArray[0].trim(), Integer.parseInt(tempArray[1].trim())));
-			}
-		}
+		Utils.readStorehouse(reader, zaisluSandelys, anglis);
 		
 		List<Vaikas> vaikuNorai = new ArrayList<Vaikas>();
 		reader = Utils.startScanner("C:\\Users\\kosta\\eclipse-workspace\\Uzduotys\\res\\Kaledos\\Vaikų_norai.txt");
-		reader.nextLine();
-		while(reader.hasNext()) {
-			String[] tempArray = reader.nextLine().split(",");
-			if (Integer.parseInt(tempArray[2].trim())<16) {
-				vaikuNorai.add(new Vaikas(tempArray[0].trim(), tempArray[1].trim(), Integer.parseInt(tempArray[2].trim()), tempArray[3].trim(), tempArray[4].trim()));
-			}			
-		}
+		Utils.readWishlist(reader, vaikuNorai);
 		
 		reader = Utils.startScanner("C:\\Users\\kosta\\eclipse-workspace\\Uzduotys\\res\\Kaledos\\Vaiko_charakteristika.txt");
-		reader.nextLine();
-		while(reader.hasNext()) {
-			String[] tempArray = reader.nextLine().split(",");
-			for(int i  = 0; i<vaikuNorai.size(); i++) {
-				String vaikasIsNoruSaraso = vaikuNorai.get(i).getVardas() + " " + vaikuNorai.get(i).getPavarde();
-				String vaikasIsSenelioSaraso = tempArray[0].trim() + " " + tempArray[1].trim();
-				if (vaikasIsNoruSaraso.equals(vaikasIsSenelioSaraso)) {
-					if(tempArray[2].trim().equals("Blogas") || tempArray[2].trim().equals("Bloga")) {
-						vaikuNorai.get(i).setCharakteristika(Charakteristika.BLOGAS.toString());
-					} else if (tempArray[2].trim().equals("Geras") || tempArray[2].trim().equals("Gera")) {
-						vaikuNorai.get(i).setCharakteristika(Charakteristika.GERAS.toString());
-					}
-				}					
-			}				
-		}			
+		Utils.readCharacteristrics(reader, vaikuNorai);
 		
 		List<DovanuSarasas> dovanuSarasas = new ArrayList<DovanuSarasas>();
 		List<Zaislas> uzsakymasElfams = new ArrayList<Zaislas>();
-		
-
-		for(int i  = 0; i<vaikuNorai.size(); i++) {
-			int count = 0;
-			int count2 = 0;
-			if (vaikuNorai.get(i).getCharakteristika().equals("GERAS")) {
-				for(int j = 0; j<zaisluSandelys.size(); j++) {
-					if(vaikuNorai.get(i).getKaleduNoras().equals(zaisluSandelys.get(j).getZaisloPavadinimas())) {
-						if (zaisluSandelys.get(j).getKiekis()>0 ) {							
-							zaisluSandelys.get(j).setKiekis(zaisluSandelys.get(j).getKiekis()-1);
-						} else {
-							uzsakymasElfams.add(new Zaislas(zaisluSandelys.get(j).getZaisloPavadinimas(), 1));
-						}
-						dovanuSarasas.add(new DovanuSarasas(vaikuNorai.get(i).getVardas(), vaikuNorai.get(i).getPavarde(), vaikuNorai.get(i).getAdresas(), vaikuNorai.get(i).getKaleduNoras()));
-						count++;
-					} else {
-						count2++;
-					}
-				}
-			} else if (vaikuNorai.get(i).getCharakteristika().equals("BLOGAS")) {
-				if (anglis.get(0).getKiekis()>0 ) {
-					anglis.get(0).setKiekis(anglis.get(0).getKiekis()-1);
-				}else {					
-					Utils.checkForIfExist(uzsakymasElfams, anglis.get(0).getZaisloPavadinimas());
-				}
-				dovanuSarasas.add(new DovanuSarasas(vaikuNorai.get(i).getVardas(), vaikuNorai.get(i).getPavarde(), vaikuNorai.get(i).getAdresas(),"Anglių maišas"));
-				count++;
-			}
-			
-			if (count == 0) {
-				dovanuSarasas.add(new DovanuSarasas(vaikuNorai.get(i).getVardas(), vaikuNorai.get(i).getPavarde(), vaikuNorai.get(i).getAdresas(), vaikuNorai.get(i).getKaleduNoras()));
-			}	
-			
-			
-			System.out.println(count2);
-			if (count2 == zaisluSandelys.size()) {
-				Utils.checkForIfExist(uzsakymasElfams, vaikuNorai.get(i).getKaleduNoras());				
-			}
-		}	
-
+		Utils.fillGiftList(vaikuNorai, zaisluSandelys, uzsakymasElfams, anglis, dovanuSarasas);
 	
-		
+		System.out.println("Dovanų sąrašas");
+		Utils.printGiftList(dovanuSarasas);
 		Utils.writeToFile(dovanuSarasas);    //C:\\Users\\kosta\\eclipse-workspace\\Uzduotys\\res\\Kaledos\\Dovanu_sarasas.txt
 		
+		System.out.println("Užsakymas elfams");
+		Utils.printToys(uzsakymasElfams);
 		Utils.writeToFileUzsakymas(uzsakymasElfams);    //C:\\Users\\kosta\\eclipse-workspace\\Uzduotys\\res\\Kaledos\\Gamybos_sarasas.txt
 		
+		System.out.println("Žaislai, kuriuos reikia išrinkti");
+		Utils.printToys(zaisluSandelys);
 		Utils.writeToFileUzsakymas(zaisluSandelys);    //C:\\Users\\kosta\\eclipse-workspace\\Uzduotys\\res\\Kaledos\\Zaislai_skirti_isardyti.txt	
-
+		
+		reader.close();
 
 	}
 
--- src/lt/bt/Kaledu_senelis/Utils.java
+++ src/lt/bt/Kaledu_senelis/Utils.java
@@ -9,8 +9,7 @@ import java.io.IOException;
 import java.util.List;
 import java.util.Scanner;
 
-import lt.bt.Testas.objektai_temperatura.Matavimai;
-import lt.bt.Testas.objektai_temperatura.Temperatura;
+import lt.bt.Kaledu_senelis.Charakteristika;
 
 public class Utils {
 	public static String enterPath() {		
@@ -59,6 +58,13 @@ public class Utils {
 		System.out.println("Write done");
 	}
 	
+	public static void printGiftList(List<DovanuSarasas> dovanuSarasas) {		
+		System.out.println("Vardas, Pavardė, Adresas, Dovana" +"\r\n");
+		for (int i = 0; i<dovanuSarasas.size(); i++) {
+			System.out.println(dovanuSarasas.get(i).getVardas() + ", " + dovanuSarasas.get(i).getPavarde() + ", " + dovanuSarasas.get(i).getAdresas() + ", " + dovanuSarasas.get(i).getDovana() +"\r\n");
+		}
+	}
+	
 	public static void writeToFileUzsakymas(List<Zaislas> zaislai) throws IOException {
 		String fileWrite = enterPath();
 		FileWriter fw = new FileWriter(fileWrite);
@@ -73,6 +79,13 @@ public class Utils {
 		fw.close();
 		System.out.println("Write done");
 		
+	}
+	
+	public static void printToys(List<Zaislas> zaislai) {
+		System.out.println("Žaislas, Kiekis" +"\r\n");
+		for (int i = 0; i<zaislai.size(); i++) {
+			System.out.println(zaislai.get(i).getZaisloPavadinimas() + ", " + zaislai.get(i).getKiekis() +"\r\n");
+		}			
 	}
 	
 	public static void checkForIfExist(List <Zaislas> sarasas, String name) {
@@ -90,6 +103,82 @@ public class Utils {
 		} else {
 			sarasas.add(new Zaislas(name, 1));
 		}	
+	}
+	
+	public static void readStorehouse(Scanner reader, List<Zaislas> zaisluSandelys, List<Zaislas> anglis) {
+		reader.nextLine();
+		while(reader.hasNext()) {
+			String[] tempArray = reader.nextLine().split(",");
+			if (!tempArray[0].trim().equals("Anglių maišas")) {
+				zaisluSandelys.add(new Zaislas(tempArray[0].trim(), Integer.parseInt(tempArray[1].trim())));
+			} else {
+				anglis.add(new Zaislas(tempArray[0].trim(), Integer.parseInt(tempArray[1].trim())));
+			}
+		}
+	}
+	
+	public static void readWishlist(Scanner reader, List<Vaikas> vaikuNorai) {
+		reader.nextLine();
+		while(reader.hasNext()) {
+			String[] tempArray = reader.nextLine().split(",");
+			if (Integer.parseInt(tempArray[2].trim())<16) {
+				vaikuNorai.add(new Vaikas(tempArray[0].trim(), tempArray[1].trim(), Integer.parseInt(tempArray[2].trim()), tempArray[3].trim(), tempArray[4].trim()));
+			}			
+		}
+	}
+	
+	public static void readCharacteristrics(Scanner reader, List<Vaikas> vaikuNorai) {
+		reader.nextLine();
+		while(reader.hasNext()) {
+			String[] tempArray = reader.nextLine().split(",");
+			for(int i  = 0; i<vaikuNorai.size(); i++) {
+				String vaikasIsNoruSaraso = vaikuNorai.get(i).getVardas() + " " + vaikuNorai.get(i).getPavarde();
+				String vaikasIsSenelioSaraso = tempArray[0].trim() + " " + tempArray[1].trim();
+				if (vaikasIsNoruSaraso.equals(vaikasIsSenelioSaraso)) {
+					if(tempArray[2].trim().equals("Blogas") || tempArray[2].trim().equals("Bloga")) {
+						vaikuNorai.get(i).setCharakteristika(Charakteristika.BLOGAS.toString());
+					} else if (tempArray[2].trim().equals("Geras") || tempArray[2].trim().equals("Gera")) {
+						vaikuNorai.get(i).setCharakteristika(Charakteristika.GERAS.toString());
+					}
+				}					
+			}				
+		}
+	}
+	
+	public static void fillGiftList(List<Vaikas> vaikuNorai, List<Zaislas> zaisluSandelys, List<Zaislas> uzsakymasElfams, List<Zaislas> anglis, List<DovanuSarasas> dovanuSarasas) {
+		for(int i  = 0; i<vaikuNorai.size(); i++) {
+			int count = 0;
+			int count2 = 0;
+			if (vaikuNorai.get(i).getCharakteristika().equals("GERAS")) {
+				for(int j = 0; j<zaisluSandelys.size(); j++) {
+					if(vaikuNorai.get(i).getKaleduNoras().equals(zaisluSandelys.get(j).getZaisloPavadinimas())) {
+						if (zaisluSandelys.get(j).getKiekis()>0 ) {							
+							zaisluSandelys.get(j).setKiekis(zaisluSandelys.get(j).getKiekis()-1);
+						} else {
+							uzsakymasElfams.add(new Zaislas(zaisluSandelys.get(j).getZaisloPavadinimas(), 1));
+						}
+						dovanuSarasas.add(new DovanuSarasas(vaikuNorai.get(i).getVardas(), vaikuNorai.get(i).getPavarde(), vaikuNorai.get(i).getAdresas(), vaikuNorai.get(i).getKaleduNoras()));
+						count++;
+					} else {
+						count2++;
+					}
+				}
+			} else if (vaikuNorai.get(i).getCharakteristika().equals("BLOGAS")) {
+				if (anglis.get(0).getKiekis()>0 ) {
+					anglis.get(0).setKiekis(anglis.get(0).getKiekis()-1);
+				}else {					
+					Utils.checkForIfExist(uzsakymasElfams, anglis.get(0).getZaisloPavadinimas());
+				}
+				dovanuSarasas.add(new DovanuSarasas(vaikuNorai.get(i).getVardas(), vaikuNorai.get(i).getPavarde(), vaikuNorai.get(i).getAdresas(),"Anglių maišas"));
+				count++;
+			}			
+			if (count == 0) {
+				dovanuSarasas.add(new DovanuSarasas(vaikuNorai.get(i).getVardas(), vaikuNorai.get(i).getPavarde(), vaikuNorai.get(i).getAdresas(), vaikuNorai.get(i).getKaleduNoras()));
+			}
+			if (count2 == zaisluSandelys.size()) {
+				Utils.checkForIfExist(uzsakymasElfams, vaikuNorai.get(i).getKaleduNoras());				
+			}
+		}
 	}
 
 }
